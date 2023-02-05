3437c5dc47d01f12f3677261678bb9ae5c892482
Zhen Zhang <git@zhen-zhang.com>
2023-02-05 11:24:34 -0800

    Unify users API

--- chafan_core/tests/app/api/api_v1/test_users.py
+++ chafan_core/tests/app/api/api_v1/test_users.py
@@ -36,85 +36,56 @@ def test_get_users_normal_user_me(
     assert current_user["email"] == EMAIL_TEST_USER
 
 
-def test_create_user_new_email(
-    client: TestClient, superuser_token_headers: dict, db: Session
+def create_invitation_uuid(client: TestClient) -> str:
+    r = client.get(
+        f"{settings.API_V1_STR}/invitation-links/daily",
+    )
+    assert 200 <= r.status_code < 300, r.text
+    return r.json()["uuid"]
+
+
+def get_open_user_account_response(
+    client: TestClient, username: str, password: str, invitation_uuid: str
 ) -> None:
-    username = random_email()
-    password = random_password()
-    data = {"email": username, "password": password.get_secret_value()}
-    r = client.post(
-        f"{settings.API_V1_STR}/users/",
-        headers=superuser_token_headers,
+    data = {
+        "email": username,
+        "password": password,
+        "handle": random_short_lower_string(),
+        "code": "dev",
+        "invitation_link_uuid": invitation_uuid,
+    }
+    return client.post(
+        f"{settings.API_V1_STR}/open-account",
         json=data,
     )
-    assert 200 <= r.status_code < 300
+
+
+def test_create_user_new_email(client: TestClient, db: Session) -> None:
+    invitation_uuid = create_invitation_uuid(client)
+    username = random_email()
+    password = random_password()
+    r = get_open_user_account_response(
+        client, username, password.get_secret_value(), invitation_uuid
+    )
+    assert 200 <= r.status_code < 300, r.text
     created_user = r.json()
+
     user = crud.user.get_by_email(db, email=username)
     assert user
     assert user.email == created_user["email"]
 
 
-def test_get_existing_user(
-    client: TestClient, superuser_token_headers: dict, db: Session
-) -> None:
-    username = random_email()
-    password = random_password()
-    user_in = UserCreate(
-        email=username, password=password, handle=random_short_lower_string()
-    )
-    user = crud.user.create(db, obj_in=user_in)
-    user_id = user.id
-    r = client.get(
-        f"{settings.API_V1_STR}/users/{user_id}",
-        headers=superuser_token_headers,
-    )
-    assert 200 == r.status_code
-    api_user = r.json()
-    existing_user = crud.user.get_by_email(db, email=username)
-    assert existing_user
-    assert existing_user.email == api_user["email"]
-
-
-def test_create_user_existing_username(
-    client: TestClient, superuser_token_headers: dict, db: Session
-) -> None:
+def test_create_user_existing_username(client: TestClient, db: Session) -> None:
+    invitation_uuid = create_invitation_uuid(client)
     username = random_email()
     password = random_password()
     user_in = UserCreate(
         email=username, password=password, handle=random_short_lower_string()
     )
     crud.user.create(db, obj_in=user_in)
-    data = {"email": username, "password": password.get_secret_value()}
-    r = client.post(
-        f"{settings.API_V1_STR}/users/",
-        headers=superuser_token_headers,
-        json=data,
+    r = get_open_user_account_response(
+        client, username, password.get_secret_value(), invitation_uuid
     )
+    assert r.status_code == 401
     created_user = r.json()
-    assert r.status_code == 400
     assert "_id" not in created_user
-
-
-def test_retrieve_users(
-    client: TestClient, superuser_token_headers: dict, db: Session
-) -> None:
-    username = random_email()
-    password = random_password()
-    user_in = UserCreate(
-        email=username, password=password, handle=random_short_lower_string()
-    )
-    crud.user.create(db, obj_in=user_in)
-
-    username2 = random_email()
-    password2 = random_password()
-    user_in2 = UserCreate(
-        email=username2, password=password2, handle=random_short_lower_string()
-    )
-    crud.user.create(db, obj_in=user_in2)
-
-    r = client.get(f"{settings.API_V1_STR}/users/", headers=superuser_token_headers)
-    all_users = r.json()
-
-    assert len(all_users) > 1
-    for item in all_users:
-        assert "email" in item
