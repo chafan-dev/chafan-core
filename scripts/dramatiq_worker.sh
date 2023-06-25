20c9c4841110653bd194d20586a3bf9e7bc2edb1
Zhen Zhang <git@zhen-zhang.com>
2023-06-25 10:48:06 -0700

    Update worker env

--- scripts/dramatiq_worker.sh
+++ scripts/dramatiq_worker.sh
@@ -5,6 +5,8 @@ export dramatiq_prom_host=0.0.0.0
 export dramatiq_prom_port=9191
 export prometheus_multiproc_dir=/tmp/dramatiq-prometheus
 export dramatiq_prom_db=$prometheus_multiproc_dir
+export DB_SESSION_POOL_SIZE=2
+export DB_SESSION_POOL_MAX_OVERFLOW_SIZE=1
 
 mkdir -p $prometheus_multiproc_dir
 
