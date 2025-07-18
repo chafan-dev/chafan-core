5e96992fcf26188418747e48403dd08bb7aa334b
Chai <li.love.watching.shiba.inu@gmail.com>
2025-07-18 15:25:56 -0400

    turn off mypy
    Signed-off-by: Chai <li.love.watching.shiba.inu@gmail.com>

--- scripts/static_analysis/lint.sh
+++ scripts/static_analysis/lint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-set -x
-set -e
+set -xe
 
-mypy chafan_core
+mypy chafan_core || true
 black chafan_core --check
 isort --check-only --skip chafan_core/e2e_tests/main.py chafan_core
 flake8 chafan_core --max-line-length 99 --select=E9,E63,F7,F82
