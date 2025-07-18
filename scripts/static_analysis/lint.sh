a9c638283ed8846a26e45a02cd7b926878d18f89
Chai <li.love.watching.shiba.inu@gmail.com>
2025-07-18 15:28:30 -0400

    turn off black
    Signed-off-by: Chai <li.love.watching.shiba.inu@gmail.com>

--- scripts/static_analysis/lint.sh
+++ scripts/static_analysis/lint.sh
@@ -3,6 +3,6 @@
 set -xe
 
 mypy chafan_core || true
-black chafan_core --check
+black chafan_core --check || true
 isort --check-only --skip chafan_core/e2e_tests/main.py chafan_core
 flake8 chafan_core --max-line-length 99 --select=E9,E63,F7,F82
