67ef034ba75b2b83297771ae6675c8cc8a06a8e3
Chai <li.love.watching.shiba.inu@gmail.com>
2025-08-10 11:28:39 -0400

    add dramatiq

--- scripts/launch_serv/2_redis_screen.sh
+++ scripts/launch_serv/2_redis_screen.sh
@@ -1,7 +1,6 @@
 base=$(git rev-parse --show-toplevel)
 cd "$base"
 
-#screen -S redis bash -c "bash $base/scripts/launch_serv/_redis.sh"
 screen -S redis bash -c \
     "nix develop --command $base/scripts/launch_serv/_redis.sh"
 
