a3679010b650619345d55cb6e16547323cffa5f4
Chai <li.love.watching.shiba.inu@gmail.com>
2025-11-22 16:23:04 -0500

    don't overwrite test env
    Signed-off-by: Chai <li.love.watching.shiba.inu@gmail.com>

--- chafan_core/tests/conftest.py
+++ chafan_core/tests/conftest.py
@@ -1,6 +1,6 @@
 from pytest_trio.enable_trio_mode import *
 
-# Set Mock env config
+# Set Mock env config (only if not already set)
 import os
 mock_env = {
 "DATABASE_URL" : "stub_url",
@@ -8,7 +8,8 @@ mock_env = {
 "SERVER_HOST" : "stub_server_host",
 }
 for k,v in mock_env.items():
-    os.environ[k] = v
+    if k not in os.environ:
+        os.environ[k] = v
 
 # End of Mock env config
 
