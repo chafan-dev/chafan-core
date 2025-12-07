cec0cc42fb62a34bd935282551842ed03afb2f8c
Chai <li.love.watching.shiba.inu@gmail.com>
2025-12-07 16:57:43 -0500

    crud test now is async
    Signed-off-by: Chai <li.love.watching.shiba.inu@gmail.com>

--- chafan_core/tests/app/crud/test_crud_user.py
+++ chafan_core/tests/app/crud/test_crud_user.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
@@ -11,48 +12,53 @@ from chafan_core.tests.utils.utils import (
 )
 
 
-def test_create_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_create_user(db: Session) -> None:
     email = random_email()
     password = random_password()
     user_in = UserCreate(
         email=email, password=password, handle=random_short_lower_string()
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     assert user.email == email
     assert hasattr(user, "hashed_password")
 
 
-def test_authenticate_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_authenticate_user(db: Session) -> None:
     email = random_email()
     password = random_password()
     user_in = UserCreate(
         email=email, password=password, handle=random_short_lower_string()
     )
-    user = crud.user.create(db, obj_in=user_in)
-    authenticated_user = crud.user.authenticate(db, email=email, password=password)
+    user = await crud.user.create(db, obj_in=user_in)
+    authenticated_user = await crud.user.authenticate(db, email=email, password=password)
     assert authenticated_user
     assert user.email == authenticated_user.email
 
 
-def test_not_authenticate_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_not_authenticate_user(db: Session) -> None:
     email = random_email()
     password = random_password()
-    user = crud.user.authenticate(db, email=email, password=password)
+    user = await crud.user.authenticate(db, email=email, password=password)
     assert user is None
 
 
-def test_check_if_user_is_active(db: Session) -> None:
+@pytest.mark.trio
+async def test_check_if_user_is_active(db: Session) -> None:
     email = random_email()
     password = random_password()
     user_in = UserCreate(
         email=email, password=password, handle=random_short_lower_string()
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     is_active = crud.user.is_active(user)
     assert is_active is True
 
 
-def test_check_if_user_is_active_inactive(db: Session) -> None:
+@pytest.mark.trio
+async def test_check_if_user_is_active_inactive(db: Session) -> None:
     email = random_email()
     password = random_password()
     user_in = UserCreate(
@@ -60,12 +66,13 @@ def test_check_if_user_is_active_inactive(db: Session) -> None:
         email=email,
         password=password,
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     is_active = crud.user.is_active(user)
     assert is_active
 
 
-def test_check_if_user_is_superuser(db: Session) -> None:
+@pytest.mark.trio
+async def test_check_if_user_is_superuser(db: Session) -> None:
     email = random_email()
     password = random_password()
     user_in = UserCreate(
@@ -74,23 +81,25 @@ def test_check_if_user_is_superuser(db: Session) -> None:
         password=password,
         is_superuser=True,
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     is_superuser = crud.user.is_superuser(user)
     assert is_superuser is True
 
 
-def test_check_if_user_is_superuser_normal_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_check_if_user_is_superuser_normal_user(db: Session) -> None:
     username = random_email()
     password = random_password()
     user_in = UserCreate(
         handle=random_short_lower_string(), email=username, password=password
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     is_superuser = crud.user.is_superuser(user)
     assert is_superuser is False
 
 
-def test_get_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_get_user(db: Session) -> None:
     password = random_password()
     username = random_email()
     user_in = UserCreate(
@@ -99,14 +108,15 @@ def test_get_user(db: Session) -> None:
         password=password,
         is_superuser=True,
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     user_2 = crud.user.get(db, id=user.id)
     assert user_2
     assert user.email == user_2.email
     assert jsonable_encoder(user) == jsonable_encoder(user_2)
 
 
-def test_update_user(db: Session) -> None:
+@pytest.mark.trio
+async def test_update_user(db: Session) -> None:
     password = random_password()
     email = random_email()
     user_in = UserCreate(
@@ -115,7 +125,7 @@ def test_update_user(db: Session) -> None:
         password=password,
         is_superuser=True,
     )
-    user = crud.user.create(db, obj_in=user_in)
+    user = await crud.user.create(db, obj_in=user_in)
     new_password = random_password()
     user_in_update = UserUpdate(password=new_password, is_superuser=True)
     crud.user.update(db, db_obj=user, obj_in=user_in_update)
