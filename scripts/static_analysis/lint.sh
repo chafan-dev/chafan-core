91dfce6a51e17d73d2de0c946e4684d9a5df471d
Chai <li.love.watching.shiba.inu@gmail.com>
2026-01-17 23:36:07 -0500

    isort --check-only --skip chafan_core/tests chafan_core
    Signed-off-by: Chai <li.love.watching.shiba.inu@gmail.com>

--- scripts/static_analysis/lint.sh
+++ scripts/static_analysis/lint.sh
@@ -4,5 +4,5 @@ set -xe
 
 mypy chafan_core
 black chafan_core --check
-isort --check-only --skip chafan_core/e2e_tests/main.py chafan_core || true
+isort --check-only --skip chafan_core/tests chafan_core
 flake8 chafan_core --max-line-length 99 --select=E9,E63,F7,F82 || true
