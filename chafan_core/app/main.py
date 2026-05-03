448c2b7a18d4e0663541147e88314c91712f78f9
chai-inu <li.love.watching.shiba.inu@gmail.com>
2026-05-02 22:44:11 -0400

    Wire APScheduler jobs: fill_missing_keywords, refresh_karmas, run_deliver_notification (#111)

--- chafan_core/app/main.py
+++ chafan_core/app/main.py
@@ -54,6 +54,9 @@ from chafan_core.app.task import (
         write_view_count_to_db,
         refresh_search_index,
 )
+from chafan_core.app.text_analysis import fill_missing_keywords_task
+from chafan_core.scheduled.deliver_notifications import run_deliver_notification_task
+from chafan_core.scheduled.lib import refresh_karmas
 
 args: MutableMapping[str, Optional[Any]] = {}
 if is_dev():
@@ -127,11 +130,23 @@ def set_up_scheduled_tasks():
         scheduler.add_job(
                 write_view_count_to_db,
                 trigger=IntervalTrigger(minutes=settings.SCHEDULED_TASK_UPDATE_VIEW_COUNT_MINUTES),
-                name="write_new_activities_to_feeds")
+                name="write_view_count_to_db")
         scheduler.add_job(
                 refresh_search_index,
                 trigger=IntervalTrigger(hours=settings.SCHEDULED_TASK_REFRESH_SEARCH_INDEX_HOURS),
                 name="refresh_search_index")
+        scheduler.add_job(
+                fill_missing_keywords_task,
+                trigger=IntervalTrigger(hours=settings.SCHEDULED_TASK_FILL_MISSING_KEYWORDS_HOURS),
+                name="fill_missing_keywords_task")
+        scheduler.add_job(
+                refresh_karmas,
+                trigger=IntervalTrigger(hours=settings.SCHEDULED_TASK_REFRESH_KARMAS_HOURS),
+                name="refresh_karmas")
+        scheduler.add_job(
+                run_deliver_notification_task,
+                trigger=IntervalTrigger(hours=settings.SCHEDULED_TASK_DELIVER_NOTIFICATIONS_HOURS),
+                name="run_deliver_notification_task")
         scheduler.start()
         logger.info("Set up scheduled tasks")
     else:
